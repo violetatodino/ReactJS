9b6ae7cb3decb6410eeccbde3a5d9f148fce0e12
violetatodino <todinovioleta@gmail.com>
2022-02-19 14:49:56 -0300

    clase2

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = () => {
+  return (
+    <div className='flex justify-center'>¡Bienvenidos a FELINA!</div>
+  )
+}
+
+export default ItemListContainer
